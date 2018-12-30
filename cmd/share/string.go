7222dbed8aca1d4e1704f13faab1f64d56b846be
Susant Sahani <ssahani@gmail.com>
2018-12-30 19:36:25 +0530

    Make string functions O(1)

--- cmd/share/string.go
+++ cmd/share/string.go
@@ -4,23 +4,32 @@ import(
 	"errors"
 )
 
-func StringContains(a []string, s string) bool {
-	for _, n := range a {
-		if s == n {
-			return true
-		}
+func StringContains(list []string, s string) bool {
+	set := make(map[string]int)
+
+	for k, v := range list {
+		set[v] = k
+	}
+
+	if set[s] != 0 {
+		return true
 	}
 
 	return false
 }
 
-func StringDeleteSlice(a []string, s string) ([]string, error) {
-	for i, n := range a {
-		if s == n {
-			a = append(a[:i], a[i+1:]...)
-			return a, nil
-		}
+func StringDeleteSlice(list []string, s string) ([]string, error) {
+	set := make(map[string]int)
+
+	for k, v := range list {
+		set[v] = k
 	}
 
-	return nil, errors.New("slice not found")
+	i, v := set[s]
+	if v {
+		list = append(list[:i], list[i+1:]...)
+		return list, nil
+	}
+
+	return nil, errors.New("Slice not found")
 }
